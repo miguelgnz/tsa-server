06437544e29ad223c8e9162ce6fc430824ba8e51
Miguel González <mgx.gon@gmail.com>
2025-01-22 10:27:55 -0600

    update CORS configuration and modify Vercel routing for improved deployment

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(
     origin: [
       'http://localhost:3001',
       'https://mdt-ui.vercel.app',
-      'https://mdt-ui-3tva-miguelgnzs-projects.vercel.app/',
-      'https://mdt-ui-3tva-git-main-miguelgnzs-projects.vercel.app/',
-      'https://mdt-ui-3tva-git-develop-miguelgnzs-projects.vercel.app/'
+      'https://mdt-ui-3tva-miguelgnzs-projects.vercel.app',
+      'https://mdt-ui-3tva-git-main-miguelgnzs-projects.vercel.app',
+      'https://mdt-ui-3tva-git-develop-miguelgnzs-projects.vercel.app'
     ],
     methods: ['DELETE', 'GET', 'PATCH', 'POST', 'PUT'],
     credentials: true
